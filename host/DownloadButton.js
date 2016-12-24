741c4fb7fa378ffc88a834e5fdcb8f460143b948
hayarasu308 <yunoyunowatawata3@yahoo.co.jp>
2016-12-24 16:24:06 +0900

    add bom

--- host/DownloadButton.js
+++ host/DownloadButton.js
@@ -21,8 +21,8 @@ const DownloadButton = ({ participants, question_text, style, disabled }) => (
       + "登録者数," + Object.keys(participants).length + "\n"
       + "ID,1問目の回答,2問目の回答\n"
       + Object.keys(participants).map(id => [id, (participants[id].question1 != 0)? question_text["question1"].title[participants[id].question1 - 1] : "未回答", (participants[id].question2 != 0)? question_text["question2"].title[participants[id].question2 - 1] : "未回答"].join(',')).join("\n")
-      
-      var blob = new Blob([content]);
+      var bom = new Uint8Array([0xEF, 0xBB, 0xBF]);
+      var blob = new Blob([bom,content]);
       var url = window.URL || window.webkitURL;
       var blobURL = url.createObjectURL(blob);
 
